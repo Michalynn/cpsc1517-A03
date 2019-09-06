48cd4bd72bf1e594c842bc3cf98b01f58ba8b4f4
Michalynn <Riseline.Ms@gmail.com>
2019-09-06 11:43:31 -0600

    create Fence_Panel OOPs

---
+++ OOPs_Review/OOPs_Review/Fence_Panel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Review
+{
+
+    //by default all classes are PRIVATE
+    //for this to be used by an outside user
+    //  you MUST alter the permission for this class
+    //  you WILL use public
+    public class Fence_Panel
+    {
+    }
+}
